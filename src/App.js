bddbbee4bf040725d0d5c1c36675634c08121277
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-01-26 19:46:09 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import NavBar from "./NavBar/NavBar";
+
 function App() {
-  return <div className="App">HelloWorld</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;

ce43ddd09de65f8753b15795f5dd769202bf2b4c
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-02-09 11:15:17 +0300

    add : Avatar

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -11,27 +11,33 @@ import github from "react-useanimations/lib/github";
 import linkedin from "react-useanimations/lib/linkedin";
 import arrowDownCircle from "react-useanimations/lib/arrowDownCircle";
 
-function Particle() {
+//Lottie
+
+import Lottie from "react-lottie-player";
+import avatar from "../../assets/avatar/avatar.json";
+
+function Hero() {
+  console.log(avatar);
   const particlesInit = useCallback((main) => {
     loadFull(main);
   }, []);
   return (
-    <div className="  ">
+    <div className="h-[100vh] lg:flex   ">
       <Particles
         init={particlesInit}
-        className="h-[600px]    "
+        className="h-[100%]    "
         options={particlesOptions}
       />
       <div className="     p-4    ">
-        <h1 className="    text-[50px] transition-all duration-1000    ">
+        <h1 className="    text-[55px] lg:text-[70px] transition-all duration-1000    ">
           Hi There,
-          <br /> I'm Rebaz Mohsen
+          <br /> I'm Rebaz <span className="text-orange-400"> Mohsen </span>
         </h1>
-        <div className=" text-[25px]   ">
+        <div className=" transition-all duration-1000 text-[25px] lg:text-[35px]    ">
           <p>I'm A Front-End Developer</p>
         </div>
         <a className="" href="#About">
-          <div className="    flex   bg-[#2506ad]    hover:bg-[#230F7E] shadow-md shadow-[#2506ad] p-[10px] pl-[30px] m-4 w-[167px] h-[45px] rounded-3xl text-white  font-semibold ml-2 ">
+          <div className="    flex   bg-[#2506ad]    hover:bg-[#230F7E] shadow-md shadow-[#2506ad] p-[10px] pl-[30px] m-4 w-[167px] h-[45px]  rounded-3xl text-white  font-semibold ml-2 ">
             <div className="  ">About Me</div>
             <UseAnimations
               className={` fill-gray-50  m-0  `}
@@ -61,8 +67,17 @@ function Particle() {
           </a>
         </div>
       </div>
+      <div className="     ">
+        <Lottie
+          className="w-[100%] h-[500px] lg:h-[800px] "
+          loop
+          animationData={avatar}
+          play
+          /*  style={{ width: 400, height: 400 }} */
+        />
+      </div>
     </div>
   );
 }
 
-export default Particle;
+export default Hero;

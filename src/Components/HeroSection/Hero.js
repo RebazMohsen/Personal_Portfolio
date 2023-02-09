d5e380b65a0d070af0475bcc620cf7a07f73ca26
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-02-09 09:50:39 +0300

    add : Github and LinkedIn

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -4,8 +4,14 @@ import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import particlesOptions from "./particles";
 import "./Hero.css";
+
+// Animated Icons
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github";
+import linkedin from "react-useanimations/lib/linkedin";
+import arrowDownCircle from "react-useanimations/lib/arrowDownCircle";
+
 function Particle() {
-  console.log(particlesOptions);
   const particlesInit = useCallback((main) => {
     loadFull(main);
   }, []);
@@ -24,11 +30,36 @@ function Particle() {
         <div className=" text-[25px]   ">
           <p>I'm A Front-End Developer</p>
         </div>
-        <a href="#About">
-          <button className=" bg-[#2506ad]   hover:bg-[#230F7E] shadow-md shadow-[#2506ad] p-2 m-4 w-[167px] h-[45px] rounded-3xl text-white  font-semibold ml-2 ">
-            About Me
-          </button>
+        <a className="" href="#About">
+          <div className="    flex   bg-[#2506ad]    hover:bg-[#230F7E] shadow-md shadow-[#2506ad] p-[10px] pl-[30px] m-4 w-[167px] h-[45px] rounded-3xl text-white  font-semibold ml-2 ">
+            <div className="  ">About Me</div>
+            <UseAnimations
+              className={` fill-gray-50  m-0  `}
+              animation={arrowDownCircle}
+              size={30}
+              strokeColor="white"
+              loop={false}
+            />
+          </div>
         </a>
+        <div className="pt-2 pb-2 flex ">
+          <a href="https://github.com/RebazMohsen">
+            <UseAnimations
+              className="border-2 rounded-full p-2 hover:bg-slate-300 "
+              animation={github}
+              size={55}
+              loop={false}
+            />
+          </a>
+          <a href="https://iq.linkedin.com/in/rebaz-mohsen-a68817170 ">
+            <UseAnimations
+              className="border-2 rounded-full p-2 hover:bg-slate-300"
+              animation={linkedin}
+              size={55}
+              loop={false}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );

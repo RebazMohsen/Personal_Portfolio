e3dbe905622b5f79459311797731d1dd51bb1d5e
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-02-13 19:51:41 +0300

    Add: Skills Structure

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,6 +7,10 @@ export default function About() {
         <h1 className="text-4xl text-center">About Me</h1>
         <div className="font-serif   flex p-4 mt-10  text-4xl mx-auto w-[60%] text-justify   ">
           <div>
+            <h1 className="font-bold text-2xl border-2 text-center rounded-xl">
+              I'm Rebaz Front-End Developer
+            </h1>
+            <br />
             <h1 className=" ">
               I am a Bachelor's degree holder in department of Engineering and
               Computer Science and I was one of the top students in my
@@ -14,7 +18,8 @@ export default function About() {
               <strong>WeCode</strong>) Boot-camp I have over six months of
               experience in
               <strong className="text-black"> React</strong> and other web
-              development technologies.
+              development technologies, I am very passionate about improving my
+              coding skills & developing WebApps & websites.
             </h1>
           </div>
         </div>
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./skills.css";
 function Skills() {
   return (
-    <div className="p-8 sm:p-36 text-center mx-auto h-[100vh]">
+    <div className=" bg-[#430496] p-8 text-white   text-center mx-auto h-[110vh]">
       <div>
-        <h1 className="text-4xl ">Skills</h1>
+        <h1 className="text-4xl p-4">Skills</h1>
+        <div className="flex gap-4 p-4 bg-[#2E1460]  border-2 w-[85%] mx-auto h-[90vh] rounded-3xl ">
+          <div className=" transition-all duration-500 rounded-3xl w-[189px] h-[114px] bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className=" transition-all duration-500 rounded-3xl w-[189px] h-[114px] bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+        </div>
       </div>
     </div>
   );

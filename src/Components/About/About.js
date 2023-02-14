89e336283f75122ebbc0ff0690a5750c8b12aa30
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-02-14 09:01:34 +0300

    Responsiveness: AboutMe

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function About() {
   return (
     <div id="About" className="  h-[100vh] ">
-      <div className="p-4 sm:p-36">
-        <h1 className="text-4xl text-center">About Me</h1>
-        <div className="font-serif   flex p-4 mt-10  text-4xl mx-auto w-[60%] text-justify   ">
-          <div>
-            <h1 className="font-bold text-2xl border-2 text-center rounded-xl">
+      <div className="p-8 md:p-8 mt-4">
+        <h1 className=" text-xl  md:text-4xl text-center">About Me</h1>
+        <div className="font-serif   flex p-4 mt-10 mx-auto w-[60%] text-justify   ">
+          <div className="text-md   md:text-2xl">
+            <h1 className="font-bold     border-2 text-center rounded-xl">
               I'm Rebaz Front-End Developer
             </h1>
             <br />
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -5,11 +5,50 @@ function Skills() {
     <div className=" bg-[#430496] p-8 text-white   text-center mx-auto h-[110vh]">
       <div>
         <h1 className="text-4xl p-4">Skills</h1>
-        <div className="flex gap-4 p-4 bg-[#2E1460]  border-2 w-[85%] mx-auto h-[90vh] rounded-3xl ">
-          <div className=" transition-all duration-500 rounded-3xl w-[189px] h-[114px] bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+        <div className="grid grid-cols-5 grid-rows-3  gap-4 p-4 bg-[#2E1460]  border-2 w-[85%] mx-auto h-[90vh] rounded-3xl transition-all duration-500">
+          <div className="  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
             <h1>Logo Here</h1>
           </div>
-          <div className=" transition-all duration-500 rounded-3xl w-[189px] h-[114px] bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl     bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+            <h1>Logo Here</h1>
+          </div>
+          <div className="  rounded-3xl    bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
             <h1>Logo Here</h1>
           </div>
         </div>

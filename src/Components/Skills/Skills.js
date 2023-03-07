1525bc0ac48a9a939dc6c948b560d8ce31cfb826
RebazMohsen <Rebazmohsen1111@gmail.com>
2023-03-07 18:26:15 +0300

    add skills: to skill section

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -13,66 +13,183 @@ import npm from "../../assets/icons/skillsIcons/npm.png";
 import Redux from "../../assets/icons/skillsIcons/Redux.png";
 import Tailwind from "../../assets/icons/skillsIcons/Tailwind.png";
 
+import Images from "../../assets/JSON/MyImages.json";
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function Skills() {
+  console.log(Images);
   const settings = {
+    className: "center",
+    centerMode: true,
+    centerPadding: "60px",
+    arrows: true,
+    swipeToSlide: true,
     dots: false,
     infinite: true,
+    swipe: true,
+    slidesToShow: 3,
+    initialSlide: 2,
+    lazyLoad: true,
+    autoplay: true,
+    slickPlay: true,
+    speed: 950,
+    autoplaySpeed: 4000,
 
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    speed: 2000,
-    autoplaySpeed: 2000,
     cssEase: "linear",
   };
 
   return (
-    <div className=" bg-[#430496] p-8 text-white   text-center mx-auto  h-[120vh]">
+    <div className="mb-56  p-8 text-white   text-center mx-auto  h-[50vh]">
       <div>
         <h1 className="text-4xl p-4">Skills</h1>
         {/*  <div className="grid md:grid-cols-5 md:grid-rows-3 gap-y-2 overflow-clip md:gap-4 p-4 bg-[#2E1460]  border-2 w-[85%] mx-auto h-[90vh] rounded-3xl transition-all duration-500"> */}
-        <Slider {...settings}>
-          <div className="font-sans  w-[100px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
-            <div className="  ">
-              <img className="mx-auto h-[50%]  p-16  " src={HTML} alt="logo" />
+        <Slider className="" {...settings}>
+          <div className="p-2">
+            <div className=" g-10  font-sans h-[300px]    text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto h-[100%]  p-16 "
+                  src={HTML}
+                  alt="logo"
+                />
+              </div>
+              <h1>HTML</h1>
             </div>
-            <h1>HTML</h1>
           </div>
-          <div className="font-sans w-[100px] text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
-            <div className="">
-              <img className="mx-auto h-[50%]  p-16  " src={CSS} alt="logo" />
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="">
+                <img
+                  className="mx-auto h-[100%]  p-16  "
+                  src={CSS}
+                  alt="logo"
+                />
+              </div>
+              <h1>CSS</h1>
             </div>
-            <h1>CSS</h1>
           </div>
-          <div className="font-sans  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
-            <div className="  ">
-              <img
-                className="mx-auto h-[50%]  p-16  "
-                src={Javascript}
-                alt="logo"
-              />
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto h-[100%]  p-16  "
+                  src={Javascript}
+                  alt="logo"
+                />
+              </div>
+              <h1>Javascript</h1>
             </div>
-            <h1>Javascript</h1>
           </div>
-          <div className="font-sans  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
-            <div className="  ">
-              <img
-                className="mx-auto h-[50%]  p-16    "
-                src={Reactjs}
-                alt="logo"
-              />
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto h-[100%]  p-16    "
+                  src={Reactjs}
+                  alt="logo"
+                />
+              </div>
+              <h1>ReactJS</h1>
             </div>
-            <h1>ReactJS</h1>
           </div>
-          <div className="font-sans  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
-            <div className="  ">
-              <img className="mx-auto   p-16  " src={MySql} alt="logo" />
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={MySql}
+                  alt="logo"
+                />
+              </div>
+              <h1>MySql</h1>
             </div>
-            <h1>MySql</h1>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={CPlus}
+                  alt="logo"
+                />
+              </div>
+              <h1>C++</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={Github}
+                  alt="logo"
+                />
+              </div>
+              <h1>Github</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={Bootstrap}
+                  alt="logo"
+                />
+              </div>
+              <h1>Bootstrap</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={Netlify}
+                  alt="logo"
+                />
+              </div>
+              <h1>Netlify</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={npm}
+                  alt="logo"
+                />
+              </div>
+              <h1>npm</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={Redux}
+                  alt="logo"
+                />
+              </div>
+              <h1>Redux</h1>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="font-sans h-[300px]  text-xl md:text-2xl  rounded-3xl   bg-[#050020]  hover:bg-[#1F113C]  hover:cursor-pointer">
+              <div className="  ">
+                <img
+                  className="mx-auto  h-[100%]  p-16  "
+                  src={Tailwind}
+                  alt="logo"
+                />
+              </div>
+              <h1>Tailwind</h1>
+            </div>
           </div>
         </Slider>
         {/* </div> */}
